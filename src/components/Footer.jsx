350f0f03fee638e9bd8d63ea2e23c4dbe224db22
Marko <martopole@example.com>
2023-04-08 13:49:39 +0300

    added copyright year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
 import '../styles/Footer.css';
 
 function Footer() {
+    const currentYear = new Date().getFullYear();
+    
     return (
         <div className="footer">
             <div className="container">
                 <div className="inner-footer">
-                    Copyright © 
+                    Copyright © {currentYear}
                 </div>
             </div>
         </div>
