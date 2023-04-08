34653302cfedfad17d86fc43c4ba36fda80dd8fa
Marko <martopole@example.com>
2023-04-08 14:07:14 +0300

    populated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import '../styles/Header.css';
+import mainLogo from '../assets/package-tracker-main-logo.png'
 
 function Header() {
     return (
         <div className="header">
             <div className="container">
                 <div className="inner-header">
+                    <img src={mainLogo} className='main-logo' alt="mainLogo"/>
                     Header Text
+                    <p>Login</p>
+                    <p>Register</p>
                 </div>
             </div>
         </div>
---
+++ src/components/HeaderButton.jsx
@@ -0,0 +1,11 @@
+import '../styles/HeaderButton.css';
+
+function HeaderButton() {
+    return (
+        <div className="header-button">
+            
+        </div>
+    );
+}
+
+export default HeaderButton;
