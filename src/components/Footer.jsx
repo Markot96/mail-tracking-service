46b242f39c74e87894b51cf3573044bda0e30c67
Marko <martopole@example.com>
2023-04-08 13:43:10 +0300

    Created basic setup

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import '../styles/Footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="container">
+                <div className="inner-footer">
+                    Copyright © 
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import '../styles/Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="container">
+                <div className="inner-header">
+                    Header Text
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
