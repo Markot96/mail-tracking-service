a9ec9bf33fa6ef5cac4b96861a788e13cb7b8a9d
Marko <martopole@example.com>
2023-04-08 19:26:08 +0300

    mapping post services

--- src/components/PostServices.jsx
+++ src/components/PostServices.jsx
@@ -1,16 +1,28 @@
 import '../styles/PostServices.css';
+import postalServicesData from '../data/postalServicesData'
 
 function PostServices() {
 
-  const PostService = (() => {
-    <div className='post-service'>
-    </div>
+  const PostService = ((props) => {
+    return (
+      <div className='post-service'>
+        <img src={props.logo} className='post-logo' alt='' />
+        <p>{props.name}</p>
+      </div>
+    )
   })
 
   return (
     <div className='post-services'>
       <div className='container'>
         <div className='inner-post-services'>
+          {postalServicesData.map((postalService) => (
+            <PostService
+              key={postalService.id}
+              name={postalService.name}
+              logo='../assets/novaposhta-logo.png'
+            />
+          ))}
           postal services that can be tracked
         </div>
       </div>
---
+++ src/data/postalServicesData.js
@@ -0,0 +1,14 @@
+const postalServicesData = [
+  {
+    key: 1,
+    name: 'Nova Poshta',
+    logo: '../assets/novaposhta-logo.png',
+  },
+  {
+    key: 2,
+    name: 'Ukrposhta',
+    logo: '../assets/novaposhta-logo.png',
+  }
+];
+
+export default postalServicesData;
