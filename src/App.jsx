554e5bad19f0e74c979596d5913a6dda844d3d6f
Marko <martopole@example.com>
2023-04-08 14:40:40 +0300

    added state to authorization

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer';
+import { useState } from 'react';
 
 function App() {
+
+  const [isGuest, setIsGuest] = useState();
+
+  const handleOnClick = (() => {
+    setIsGuest(!isGuest);
+  });
+
   return (
     <div className="app">
-      <Header />
+      <Header isGuest={isGuest}/>
+        <button onClick={handleOnClick}>change state</button>
       <Footer />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,35 @@
 import '../styles/Header.css';
 import mainLogo from '../assets/package-tracker-main-logo.png'
+import profileIcon from '../assets/profile-icon.png'
 
-function Header() {
+
+function Header(props) {
+    
+    const Authorization = (() => {
+        return (
+            <div className="authorization">
+                <p>Login</p>
+                <p>Register</p>
+            </div>
+        );
+    });
+
+    const MyAccount = (() => {
+        return (
+            <div className="authorization">
+                <img src={profileIcon} className="profile-icon" alt="profile icon" />
+                <p>Log out</p>
+            </div>
+        );
+    });
+
     return (
         <div className="header">
             <div className="container">
                 <div className="inner-header">
-                    <img src={mainLogo} className='main-logo' alt="mainLogo"/>
+                    <img src={mainLogo} className="main-logo" alt="mainLogo" />
                     Header Text
-                    <p>Login</p>
-                    <p>Register</p>
+                    {props.isGuest ? <Authorization/> : <MyAccount/> }
                 </div>
             </div>
         </div>
