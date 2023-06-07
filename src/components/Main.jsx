5d689915a2170586714ba850aaf84ac4515b8e69
Marko <martopole@example.com>
2023-06-07 14:34:45 +0300

    moved fetching from Main to Results section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,38 +1,14 @@
-import { useEffect, useState } from 'react';
 import '../styles/Main.css';
 import NumberInput from './NumberInput';
 import PostServices from './PostServices';
+import Results from './Results';
 
 function Main() {
-
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('https://api.novaposhta.ua/v2.0/json/')
-      // fetch('https://reqres.in/api/users')
-      .then((response) => {
-        console.log('response', response)
-        if (response.ok) {
-          return response;
-        }
-        throw new Error("Server says bad response");
-      })
-      .then((response) => setData(response.data))
-      .catch((err) => console.log(err));
-  }, []);
 
   return (
     <div className='main'>
       <NumberInput />
-      <div className='test'>
-        {data && data.map((userData) => {
-          console.log('userData.email', userData.email)
-          return <div>{userData.email}</div>
-        })}
-      </div>
-      <div className='test1'>
-        1111
-      </div>
+      <Results />
       <PostServices />
     </div>
   )
---
+++ src/components/Results.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import '../styles/Results.css';
+
+function Results() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // fetch('https://api.novaposhta.ua/v2.0/json/')
+    fetch('https://reqres.in/api/users')
+      .then((response) => {
+        console.log('response', response)
+        if (response.ok) {
+          return response;
+        }
+        throw new Error("Server says bad response");
+      })
+      .then((response) => setData(response.data))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log('data', data)
+
+  return (
+    <div className='results'>
+      <div className='container'>
+        <div className='inner-results'>
+          {data && data.map((userData) => {
+            console.log('userData.email', userData.email)
+            return <div>{userData.email}123</div>
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Results;
