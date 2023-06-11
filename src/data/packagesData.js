b88c4f0480ffb2aa726bb198d227bdd664877b5f
Marko <martopole@example.com>
2023-06-11 11:17:54 +0300

    added default data for parser

---
+++ src/data/packagesData.js
@@ -0,0 +1,136 @@
+const packagesData = {
+  "items": [
+    {
+      "package_number": "P123456",
+      "date_time_sending": "2023-06-01T10:00:00",
+      "departure_city": "New York",
+      "arrival_city": "Los Angeles",
+      "category": "Electronics",
+      "status": "Sent",
+      "sender_name": "John Doe",
+      "receiver_name": "Jane Smith",
+      "post_office_address": "123 Main Street",
+      "post_office_number": "456",
+      "arrival_date": "2023-06-05"
+    },
+    {
+      "package_number": "P234567",
+      "date_time_sending": "2023-06-02T12:30:00",
+      "departure_city": "London",
+      "arrival_city": "Paris",
+      "category": "Clothing",
+      "status": "Received",
+      "sender_name": "Alice Johnson",
+      "receiver_name": "Michael Brown",
+      "post_office_address": "456 High Street",
+      "post_office_number": "789",
+      "arrival_date": "2023-06-04"
+    },
+    {
+      "package_number": "P345678",
+      "date_time_sending": "2023-06-03T15:45:00",
+      "departure_city": "Tokyo",
+      "arrival_city": "Sydney",
+      "category": "Books",
+      "status": "On the way",
+      "sender_name": "Kenji Tanaka",
+      "receiver_name": "Emily Wilson",
+      "post_office_address": "789 Elm Avenue",
+      "post_office_number": "012",
+      "arrival_date": "2023-06-09"
+    },
+    {
+      "package_number": "P456789",
+      "date_time_sending": "2023-06-04T09:20:00",
+      "departure_city": "Berlin",
+      "arrival_city": "Rome",
+      "category": "Jewelry",
+      "status": "Arrived",
+      "sender_name": "Sophie Müller",
+      "receiver_name": "Luigi Rossi",
+      "post_office_address": "456 Oak Lane",
+      "post_office_number": "345",
+      "arrival_date": "2023-06-07"
+    },
+    {
+      "package_number": "P567890",
+      "date_time_sending": "2023-06-05T16:10:00",
+      "departure_city": "Moscow",
+      "arrival_city": "Beijing",
+      "category": "Toys",
+      "status": "Sent",
+      "sender_name": "Ivan Petrov",
+      "receiver_name": "Li Wei",
+      "post_office_address": "789 Maple Road",
+      "post_office_number": "678",
+      "arrival_date": "2023-06-10"
+    },
+    {
+      "package_number": "P678901",
+      "date_time_sending": "2023-06-06T11:30:00",
+      "departure_city": "São Paulo",
+      "arrival_city": "Mexico City",
+      "category": "Food",
+      "status": "On the way",
+      "sender_name": "Maria Silva",
+      "receiver_name": "Carlos Hernandez",
+      "post_office_address": "123 Pine Street",
+      "post_office_number": "901",
+      "arrival_date": "2023-06-12"
+    },
+    {
+      "package_number": "P789012",
+      "date_time_sending": "2023-06-07T14:50:00",
+      "departure_city": "Toronto",
+      "arrival_city": "Vancouver",
+      "category": "Sports Equipment",
+      "status": "Received",
+      "sender_name": "Emily Thompson",
+      "receiver_name": "David Wilson",
+      "post_office_address": "456 Cedar Avenue",
+      "post_office_number": "234",
+      "arrival_date": "2023-06-09"
+    },
+    {
+      "package_number": "P890123",
+      "date_time_sending": "2023-06-08T08:00:00",
+      "departure_city": "Sydney",
+      "arrival_city": "Melbourne",
+      "category": "Beauty Products",
+      "status": "Sent",
+      "sender_name": "Olivia Davis",
+      "receiver_name": "Emma Taylor",
+      "post_office_address": "789 Elm Street",
+      "post_office_number": "567",
+      "arrival_date": "2023-06-11"
+    },
+    {
+      "package_number": "P901234",
+      "date_time_sending": "2023-06-09T13:15:00",
+      "departure_city": "Cairo",
+      "arrival_city": "Dubai",
+      "category": "Home Appliances",
+      "status": "Departured",
+      "sender_name": "Mohammed Ali",
+      "receiver_name": "Ahmed Khalid",
+      "post_office_address": "123 Oak Avenue",
+      "post_office_number": "890",
+      "arrival_date": "2023-06-14"
+    },
+    {
+      "package_number": "P012345",
+      "date_time_sending": "2023-06-10T10:30:00",
+      "departure_city": "Seoul",
+      "arrival_city": "Tokyo",
+      "category": "Electronics",
+      "status": "On the way",
+      "sender_name": "Kim Soo-hyun",
+      "receiver_name": "Yamada Hiroshi",
+      "post_office_address": "456 Pine Road",
+      "post_office_number": "123",
+      "arrival_date": "2023-06-16"
+    }
+  ]
+}
+
+export default packagesData;
