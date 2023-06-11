d8ae3a9630ee3dde32fd87d2bfeecc9839f81fd3
Marko <martopole@example.com>
2023-06-11 11:40:39 +0300

    added working searching

--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -1,35 +1,61 @@
 import '../styles/NumberInput.css';
 import React, { useState } from 'react';
-import packageData from '../data/packagesData';
+import packagesData from '../data/packagesData';
 import SubmitButton from './SubmitButton';
 
 function NumberInput() {
 
   const [packageNumber, setPackageNumber] = useState('');
-  const [packageInfo, setPackageInfo] = useState(null);
+  const [packageInfo, setPackageInfo] = useState(undefined);
 
   const handleInputChange = (event) => {
     setPackageNumber(event.target.value);
   };
 
-  const handleFormSubmit = (event) => {
+  const handleClick = (event) => {
     event.preventDefault();
 
-    const foundPackage = packageData.items.find(
+    const foundPackage = packagesData.items.find(
       (item) => item.package_number === packageNumber
     );
+    console.log('foundPackage', foundPackage)
 
     setPackageInfo(foundPackage);
     setPackageNumber('');
   };
+  console.log(packageInfo)
 
   return (
     <div className='number-input'>
       <div className='container'>
         <div className='inner-number-input'>
-          <input type='text' name='name' className='text-input' />
-          <SubmitButton />
+          <input
+            type='text'
+            name='name'
+            className='text-input'
+            onChange={handleInputChange}
+          />
+          <SubmitButton onClick={handleClick} />
+          {packageInfo ? (
+            <div>
+              <h2>Package Information:</h2>
+              <p>Package Number: {packageInfo.package_number}</p>
+              <p>Date and Time of Sending: {packageInfo.date_time_sending}</p>
+              <p>City of Departure: {packageInfo.departure_city}</p>
+              <p>City of Arrival: {packageInfo.arrival_city}</p>
+              <p>Category: {packageInfo.category}</p>
+              <p>Status: {packageInfo.status}</p>
+              <p>Sender Name: {packageInfo.sender_name}</p>
+              <p>Receiver Name: {packageInfo.receiver_name}</p>
+              <p>Post Office Address: {packageInfo.post_office_address}</p>
+              <p>Post Office Number: {packageInfo.post_office_number}</p>
+              <p>Arrival Date: {packageInfo.arrival_date}</p>
+            </div>
+          ) : (
+            <p>No package information found.</p>
+          )}
         </div>
+
       </div>
     </div>
   );
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -5,7 +5,7 @@ import SearchIcon from '../assets/search-icon.png'
 function SubmitButton(props) {
 
   return (
-    <div className='submit-button'>
+    <div className='submit-button' onClick={props.onClick}>
       <img src={SearchIcon} className='search-icon' alt='search icon' />
       <p className='search-button'>Пошук</p>
     </div>
